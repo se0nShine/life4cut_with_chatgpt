2192fc3e4195ae401597d8fc6fd8f9f6580eeff3
JaeSeon <58286830+se0nShine@users.noreply.github.com>
2024-07-13 12:57:48 +0900

    Update 1st_step.py

--- 1st_step.py
+++ 1st_step.py
@@ -7,16 +7,16 @@ def capture_images():
     captured_images = []
 
     while True:
-        ret, frame = cap.read()
+        ret, frame = 
         if not ret:
             print("웹캠에서 영상을 읽을 수 없습니다.")
             break
         
         # 현재 프레임을 화면에 보여줍니다
-        cv2.imshow('Webcam', frame)
+        ('Webcam', frame)
 
         # 사용자가 'c' 키를 누르면 사진을 캡처합니다
-        if cv2.waitKey(1) & 0xFF == ord('c'):
+        if cv2.waitKey(1) & 0xFF == ord(''):
             captured_images.append()
             print(f"{len(captured_images)} 장의 사진을 캡처했습니다.")
         
